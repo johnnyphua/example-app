5009c2012e457b76f600835fa370df682329afe7
johnnyphua <119966906+johnnyphua@users.noreply.github.com>
2023-03-03 15:50:55 +0800

    Added styling

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,28 +1,32 @@
 <!DOCTYPE html>
 <html lang="en">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>{{ config('app.name') }}</title>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
-        //Include bootstrap CSS CDN here
+    <title>{{ config('app.name') }}</title>
 
-        <style>
-            body {
-                font-family: 'Nunito';
-            }
-        </style>
+    <!-- Fonts -->
+    <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@3.3.7/dist/css/bootstrap.min.css"
+        integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+    <style>
+        body {
+            font-family: 'Nunito';
+        }
+    </style>
 
-    </head>
+</head>
 
-    <body>
+<body>
 
     @yield('content')
-    
 
-    </body>
-    //Include bootstrap JS CDN here 
+
+</body>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@3.3.7/dist/js/bootstrap.min.js"
+    integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous">
+</script>
+
 </html>
